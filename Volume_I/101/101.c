6a87518fed356118dc3f7e5dd11b6e2415a51b11
Edison Wang <jawwang1129@gmail.com>
2013-03-18 18:56:30 +0800

    change input function to fgets()

--- Volume_I/101/101.c
+++ Volume_I/101/101.c
@@ -25,19 +25,19 @@ static int blocks[25][25];
 
 void init_blocks(int max){
     int i,j;
-    for ( i=0;i<max;i++){
+    for ( i=0;i<max;i++) {
         blocks[i][0]=i;
-        for( j=1;j<25;j++){
-            blocks[i][j]=i+j;
-        }
+		for(j=1;j<max;j++){
+			blocks[i][j]=-1;
+		}
     }
 }
 
 void output_blocks(int max){
     int i,j;
-    for (i=0;i<max;i++){
+    for (i=0;i<max;i++) {
         printf("%d :", i);
-        for (j=0;j<max;j++){
+        for (j=0;j<max;j++) {
             if (blocks[i][j]!=-1){
                 printf(" %d",blocks[i][j]);
             }
@@ -46,48 +46,33 @@ void output_blocks(int max){
     }
 }
 
-int isMove(char *buf){
-    return ((strstr(buf,"move")) != NULL);
-}
-
-int isPipe(char *buf){
-    return ((strstr(buf,"pipe")) != NULL);
-}
-
-int isOnto(char *buf){
-    return ((strstr(buf,"onto")) != NULL);
-}
-
-int isOver(char *buf){
-    return ((strstr(buf,"over")) != NULL);
-}
-
 int main(){
-    char command[20];
+    char command[20],first[5],second[5];
     char *temp,*buf;
-    int max;
+    int max,from,to;
     scanf("%d", &max);
+	getchar();
     init_blocks(max);
     while(1){
-        scanf("%s", command);
+		fgets(command,20,stdin);
         buf = command;
-        if (strcmp(buf,"quit") == 0){
+        if (strcmp(buf,"quit\n") == 0){
             break;
         }
-        if (isMove(buf)){
+		sscanf(buf,"%s %d %s %d", first, &from, second, &to);
+        if (strcmp(first,"move")==0){
             printf("%s\n","isMove");
         }
-        if (isPipe(buf)){
+        if (strcmp(first,"pipe")==0){
             printf("%s\n","isPipe");
         }
-        if (isOnto(buf)){
+        if (strcmp(second,"onto")==0){
             printf("%s\n","isOnto");
         }
-        if (isOver(buf)){
+        if (strcmp(second,"over")==0){
             printf("%s\n","isOver");
-        }
-
-    }
+    	}
+	}
     output_blocks(max);
     return 0;
 }
